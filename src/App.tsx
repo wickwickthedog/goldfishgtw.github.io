cff8d9d87653602493ba7aa322ef3ff78f96eef9
GoldFish <tan.harveyshyanyih@gmail.com>
2026-04-18 11:05:35 +0800

    comment imports to fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
-import Skills from './components/Skills'
-import Projects from './components/Projects'
+// import Skills from './components/Skills'
+// import Projects from './components/Projects'
 import Experience from './components/Experience'
-import Blog from './components/Blog'
-import Now from './components/Now'
-import Contact from './components/Contact'
+// import Blog from './components/Blog'
+// import Now from './components/Now'
+// import Contact from './components/Contact'
 import Footer from './components/Footer'
 
 export default function App() {
